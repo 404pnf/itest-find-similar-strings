4f4fd346b6032e096e7872788f6437f066a087dd
404 <404@example.com>
2012-12-11 10:49:08 +0800

    minor clean up

--- find_duplicate_records.rb
+++ find_duplicate_records.rb
@@ -1,4 +1,3 @@
-
 require 'csv'
 require 'test/unit'
 require 'digest/md5'
@@ -6,21 +5,19 @@ require 'pp'
 
 # USAGE: ruby script.rb inputfile
 
-=begin
-inputfile is a csv file with the format:
-id, text
-id, text
-...
 
-We process the records with following steps
-1. parse csv, make it an array of arrays
-2. make a hash with md5sum as key, and id as values, each key can has multiple values
-3. caclulate md5sum of each record's text
-4. find those keys with multiple values.  Meaning they are identical. Return the result in an array
+# inputfile is a csv file with the format:
+# id, text
+# id, text
+# ...
+# 
+# We process the records with following steps
+# 1. parse csv, make it an array of arrays
+# 2. make a hash with md5sum as key, and id as values, each key can has multiple values
+# 3. caclulate md5sum of each record's text
+# 4. find those keys with multiple values.  Meaning they are identical. Return the result in an array
 
-=end
-
-# helper functions
+# HELPER FUNCTIONS
 def remove_non_words_and_downcase str
   # \W means charcaters not in range [a-zA-z0-9_]
   # dash '-' is removed, but underscore '_' is kept
@@ -33,40 +30,36 @@ def text_md5sum str
   # Digest::MD5.hexdigest("Hello World\n")
   Digest::MD5.hexdigest(str)
 end
-# we don't need this since \W covers white spaces! 
-# !!
-# def remove_whitespace str
-#  str.gsub(/\s+/, '')
-# end
+def find_duplicate_records
+  # step 1
+  inputfile = File.read ARGV[0]
+  array = []
+  CSV.parse(inputfile) {|row| array << row}
+  #p array
 
-
-# step 1
-inputfile = File.read ARGV[0]
-array = []
-CSV.parse(inputfile) {|row| array << row}
-#p array
-
-# step 2
-db = Hash.new {|h, key| h[key]= []}
-
-# step 3
-# a sample array
-# [["0", "5av0afm7zf oocvikh1uo"], ["1", "qxuw4lzdql hh82g7jkic"]]
-array.map! {|entry|  [entry[0], text_md5sum(entry[1])]}
-# now array becomes [["0", "0721a89fcd7797946a477faea06fee65"], ["1", "7420c044640c553e27cbbf96b891ec23"]]
-array.each do |entry|
-  key, value = entry[1], entry[0].to_i
-  db[key] << value 
+  # step 2
+  db = Hash.new {|h, key| h[key]= []}
+  
+  # step 3
+  # a sample array
+  # [["0", "5av0afm7zf oocvikh1uo"], ["1", "qxuw4lzdql hh82g7jkic"]]
+  array.map! {|entry|  [entry[0], text_md5sum(entry[1])]}
+  # now array becomes [["0", "0721a89fcd7797946a477faea06fee65"], ["1", "7420c044640c553e27cbbf96b891ec23"]]
+  array.each do |entry|
+    key, value = entry[1], entry[0].to_i
+    db[key] << value 
+  end
+  #pp db
+  
+  # step 4
+  result = []
+  db.each {|key, value|  result << value if db[key].size > 1}
+  pp result
 end
-#pp db
-
-# step 4
-result = []
-db.each {|key, value|  result << value if db[key].size > 1}
-pp result
+# RUN THE MAIN FUNCTION
+find_duplicate_records  if __FILE__ == $0
 
 # tests
-
 class TestFindDuplicateEntries < Test::Unit::TestCase
 
   def test_text_md5sum
